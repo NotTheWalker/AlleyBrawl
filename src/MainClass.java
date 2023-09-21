60dccdd6e58f741a7cda7d5ef7e0ab0d5d1196ba
Caleb Morse <ST10343104@vcconnect.edu.za>
2023-09-21 20:57:10 +0200

    Last changes before submission

--- src/MainClass.java
+++ src/MainClass.java
@@ -2,5 +2,8 @@ public class MainClass {
     public static void main(String[] args) {
         WorkerClass worker = new WorkerClass();
         worker.start();
+        //didn't manage to finish the whole game, only got as far as implementing most of its backend
+        // and get it to display the scene
+        //The game is supposed to be a turn-based game where the player is supposed to defeat all the thugs in an alleyway
     }
 }
